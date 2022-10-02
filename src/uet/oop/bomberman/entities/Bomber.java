8bbbb76dca006578f27f4c753736b9290a60f1ff
tolamson2811 <100157005+tolamson2811@users.noreply.github.com>
2022-10-02 09:25:50 +0700

    add collision

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -5,6 +5,7 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import uet.oop.bomberman.BombermanGame;
 import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.entities.GameMap;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,8 +21,20 @@ public class Bomber extends Entity {
     private int movevalX;
     private int movevalY;
 
+    GameMap temp = new GameMap();
+
+    public void init() {
+        temp.ReadMap();
+        temp.LoadMap();
+    }
     public static final int SPEED = 2;
     protected  Input user_input = new Input();
+
+    public int width_frame = 30;
+
+    public int height_frame = 42;
+
+    public int TILE_SIZE = 48;
     private WALKTYPE status ;
 
     enum WALKTYPE
@@ -64,7 +77,6 @@ public class Bomber extends Entity {
 
         if(user_input.right == 1) {
             movevalX += SPEED;
-
             if(time % 9 == 0) {
                 img = Sprite.player_right_1.getFxImage();
             } else if(time % 9 == 4) {
@@ -72,9 +84,9 @@ public class Bomber extends Entity {
             } else if(time % 9 == 8) {
                 img = Sprite.player_right.getFxImage();
             }
-
             time += 0.5;
-        }else if(user_input.left == 1) {
+
+        } else if(user_input.left == 1) {
             movevalX -= SPEED;
 
             if(time % 9 == 0) {
@@ -88,8 +100,7 @@ public class Bomber extends Entity {
             time += 0.5;
         }
         if(user_input.up == 1){
-            movevalY-= SPEED;
-
+            movevalY -= SPEED;
             if(time % 9 == 0) {
                 img = Sprite.player_up_1.getFxImage();
             } else if(time % 9 == 4) {
@@ -100,7 +111,8 @@ public class Bomber extends Entity {
 
             time += 0.5;
         }else if(user_input.down == 1) {
-            movevalY +=SPEED;
+            movevalY += SPEED;
+
 
             if(time % 9 == 0) {
                 img = Sprite.player_down_1.getFxImage();
@@ -112,6 +124,8 @@ public class Bomber extends Entity {
 
             time += 0.5;
         }
+
+        checkCollisions(temp.getMAP_COLLISION());
 
         x+=movevalX;
         y+=movevalY;
@@ -179,6 +193,79 @@ public class Bomber extends Entity {
             }
         });
     }
+
+    public void checkCollisions(boolean[][] MAP_COLISION ) {
+        int x1 = 0;
+        int x2 = 0;
+
+        int y1 = 0;
+        int y2 = 0;
+
+        x1 = (this.x + this.getMovevalX()) / TILE_SIZE;
+        x2 = (this.x + this.getMovevalX() + width_frame - 1) / TILE_SIZE;
+
+        y1 = this.y / TILE_SIZE;
+        y2 = (this.y + height_frame - 1) / TILE_SIZE;
+
+        if(x1 >= 0 && y1 >= 0) {
+            if(this.getMovevalX() > 0) {
+                if(MAP_COLISION[y1][x2] || MAP_COLISION[y2][x2]) {
+                    this.x = x2 * TILE_SIZE;
+                    this.x -= width_frame + 1;
+                    this.setMovevalX(0);
+                    if(MAP_COLISION[y1][x2] && !MAP_COLISION[y2][x2]) {
+                        this.movevalY++;
+                    } else if(!MAP_COLISION[y1][x2] && MAP_COLISION[y2][x2]) {
+                        this.movevalY--;
+                    }
+                }
+            } else if(this.getMovevalX() < 0) {
+                if(MAP_COLISION[y1][x1] || MAP_COLISION[y2][x1]) {
+                    this.x = (x1 + 1) * TILE_SIZE + 1;
+                    this.setMovevalX(0);
+                }
+                if(MAP_COLISION[y1][x1] && !MAP_COLISION[y2][x1]) {
+                    this.movevalY++;
+                } else if(!MAP_COLISION[y1][x1] && MAP_COLISION[y2][x1]) {
+                    this.movevalY--;
+                }
+            }
+        }
+
+
+        //Check doc
+        x1 = this.x / TILE_SIZE;
+        x2 = (this.x + width_frame) / TILE_SIZE;
+
+        y1 = (this.y + this.getMovevalY()) / TILE_SIZE;
+        y2 = (this.y + this.getMovevalY() + height_frame - 1) / TILE_SIZE;
+
+        if(x1 >= 0 && y1 >= 0) {
+            if(this.getMovevalY() > 0) {
+                if(MAP_COLISION[y2][x1] || MAP_COLISION[y2][x2]) {
+                    this.y = y2 * TILE_SIZE;
+                    this.y -= (height_frame + 1);
+                    this.setMovevalY(0);
+                    if(MAP_COLISION[y2][x1] && !MAP_COLISION[y2][x2]) {
+                        this.movevalX++;
+                    } else if(!MAP_COLISION[y2][x2] && MAP_COLISION[y2][x2]) {
+                        this.movevalX--;
+                    }
+                }
+            } else if(this.getMovevalY() < 0) {
+                if(MAP_COLISION[y1][x1] || MAP_COLISION[y1][x2]) {
+                    this.y = (y1 + 1) * TILE_SIZE;
+                    this.setMovevalY(0);
+                }
+                if(!MAP_COLISION[y1][x1] && MAP_COLISION[y1][x2]) {
+                    this.movevalX--;
+                } else if(MAP_COLISION[y1][x1] && !MAP_COLISION[y1][x2]) {
+                    this.movevalX++;
+                }
+            }
+        }
+    }
+
     @Override
     public void update() {
         HandlingInput();
